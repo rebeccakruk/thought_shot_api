c29871fff25818b8ffe12704c0216d7d3c4271b1
rk <rebeccakruk@outlook.com>
2023-03-06 12:43:47 -0700

    sign up and signin testing

--- endpoints/user.py
+++ endpoints/user.py
@@ -1,5 +1,27 @@
 from app import app, __init__
 from flask import jsonify, make_response, request
 from dbhelpers import run_statement
-import bcrypt
 import uuid
+import bcrypt
+
+@app.post('api/user')
+def user_signup():
+    username = request.json.get("username")
+    pw = request.json.get("password")
+    if pw == None:
+        return "You must choose a password to complete signup."
+    salt = bcrypt.gensalt()
+    password = bcrypt.hashpw(pw.encode(), salt)
+    email = request.json.get("email")
+    dob = request.json.get("dob YYYY/MM/DD")
+    token = uuid.uuid4().hex
+    image = request.json.get("profile image")
+    keys = ["userID", "token"]
+    response = []
+    result = run_statement("CALL user_signup(?, ?, ?, ?)", [username, password, email, dob, token, image])
+    if (type(result) == list):
+        for data in result:
+            response.append(dict(zip(keys, data)))
+            return make_response(jsonify(response), 200)
+    else:
+        return "Please provide a valid password."
---
+++ endpoints/user_login.py
@@ -0,0 +1,28 @@
+from app import app, __init__
+from flask import jsonify, make_response, request
+from dbhelpers import run_statement
+import bcrypt
+import uuid
+
+@app.post('api/user-login')
+def user_login():
+    username = request.json.get("username")
+    result = run_statement("CALL get_id(?)", [username])
+    if (result == []):
+        return "Please enter a valid username or register as a new user."
+    if (type(result) == list):
+        password = result[0][1]
+        user_id = result[0][0]
+    pw1 = password.encode('utf-8')
+    password2 = request.json.get("password")
+    if (bcrypt.checkpw(password2.encode(), pw1)):
+        token = uuid.uuid4().hex
+        results = run_statement("CALL user_login(?, ?)", [token, user_id])
+        if results [0][0] == 1:
+            response = {
+                "userId": user_id,
+                "token" : token
+                }
+            return make_response(jsonify(response), 200)
+    else:
+        return "Please provide a valid password."
